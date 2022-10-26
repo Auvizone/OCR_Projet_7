c52eb657a1154198c113f82c98647b75f3592096
Auvizone <humphrey.preston11@gmail.com>
2022-10-26 16:53:24 +0200

    affichage des données

--- scripts/factory.js
+++ scripts/factory.js
@@ -1,3 +1,34 @@
 function recettesFactory(data) {
-    console.log(data)
+    const { name, ingredients, time, description, id } = data;
+
+    function createRecipe() {
+        console.log(time)
+        const article = document.createElement('article');
+        article.classList.add('recipe');
+
+        const a = document.createElement('a');
+        a.textContent = name;
+
+        const li = document.createElement('li');
+        ingredients.forEach((x) => {
+            const ul = document.createElement('ul');
+            ul.textContent = x.ingredient + ': ' +  x.quantity;
+            li.appendChild(ul)
+        })
+
+        const timeText = document.createElement('a');
+        timeText.textContent = time;
+
+        const descriptionText = document.createElement('p');
+        descriptionText.textContent = description
+
+        article.appendChild(a);
+        article.appendChild(li);
+        article.appendChild(timeText);
+        article.appendChild(descriptionText);
+
+
+        return (article)
+    }
+    return { name, ingredients, time, description, id, createRecipe }
 }
