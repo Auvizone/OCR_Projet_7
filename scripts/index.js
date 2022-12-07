3aca96bb7a008d249b9e709a5ba9215293d8722d
Auvizone <humphrey.preston11@gmail.com>
2022-12-07 16:43:38 +0100

    added while search in name & description

--- scripts/index.js
+++ scripts/index.js
@@ -4,8 +4,6 @@ const recettes = await response.json();
 return (recettes)
 }
 
-
-
  function displayData(data) {
     const recettesSection = document.getElementById('recettes-section');
     data.forEach((recettes) => {
@@ -31,8 +29,25 @@ return (recettes)
     if (this.value.length < 2 ) {
        return;
       } if (this.value.length > 2 ) {
-         console.log(this.value)
-   }
+        filterMeals(this.value)
+      }
+    }
+    
+    async function filterMeals(value) {
+      let mealsArray = [];
+      let x = 0;
+      const recettes = await getRecipes();
+      console.log(recettes)
+      while ( x < recettes.length) {
+        let name = recettes[x].name.toLowerCase();
+        console.log("🚀 ~ file: index.js:43 ~ filterMeals ~ name", name)
+        let description = recettes[x].description.toLowerCase();
+        if (name.includes(value) || description.includes(value)) {
+          mealsArray.push(recettes[x])
+        }
+        x++;
+      }
+      console.log(mealsArray)
  }
 
  init();
